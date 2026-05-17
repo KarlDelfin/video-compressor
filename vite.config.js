bb88b78b249cd795a7cebe7144a01611370f2598
MyreneCamingawan <yencamingawan@gmail.com>
2026-05-17 09:30:49 +0800

    updated folders

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/video-compressor',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     vue(),
     vueJsx(),
